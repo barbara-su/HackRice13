0e3d0625f23bb151ea8291eee2f3a7a432e22fde
eurethia <3366841721@qq.com>
2023-09-23 11:29:20 -0500

    prompt

--- llm_chatbot/chatter.py
+++ llm_chatbot/chatter.py
@@ -1,23 +1,32 @@
 # openai_key: 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
 import openai
 import pickle
-import json_process as json_process
+# import json_process as json_process
 
 openai.api_key = "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"
 
 tst_msg = ""
 try:
-    tst_msg = pickle.load(open("tst/save.p", "rb"))
+    tst_msg = pickle.load(open("tst/save.p", "r"))
 except:
     print("no prev message")
 
-json_file = json_process.json_to_str("tst.json")
+# json_file = json_process.json_to_str("tst.json")
 
-print(json_file)
+# print(json_file)
+
+with open('frank_test.txt', 'rb') as file:
+    # Read the entire contents of the file into a string
+    file_contents = str(file.read())
+
+# Now, file_contents contains the content of the file as a string
+print(file_contents)
 
 if len(tst_msg) > 0:
     messages = tst_msg
-else: messages = [{"role": "system", "content": "This is a json file, I will ask you question on that: " + json_file + ". Your answer should strictly follow the json file."}]
+else: messages = [{"role": "system", "content": "This is a txt file, I will ask you question on that: " + file_contents + \
+                   "\n Your answer should strictly follow the txt file.\
+                    \n If I ask you things outside of the txt file, just supplement the answer with your own knowledge."}]
 
 def update_message(messages, role, content):
     messages.append({"role": role, "content": content})
@@ -35,6 +44,7 @@ while True:
 
         messages = update_message(messages, "assistant", answer)
         print(answer)
+        print()
     except KeyboardInterrupt:
         break
 
