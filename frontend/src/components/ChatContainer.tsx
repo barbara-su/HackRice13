40288a798d165690532769eb1d7d5ebb587b3105
lixhuynh <elh7@rice.edu>
2023-09-23 15:11:58 -0500

    frontend update
    allows posting

--- frontend/src/components/ChatContainer.tsx
+++ frontend/src/components/ChatContainer.tsx
@@ -6,15 +6,35 @@ import { useState, useEffect } from "react";
 function ChatContainer() {
   const [messages, setMessages] = useState<MessageProps[]>([]);
 
-  
+  const postResult = () => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:5173/");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    let messageArr = [];
+    for (let message of messages) {
+      messageArr.push(message);
+    }
+    const data = JSON.stringify({ title: "fronttest", messages: messageArr });
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status === 200) {
+          console.log("Result sent successfully");
+        } else {
+          console.error("Error sending result:", xhr.status, xhr.statusText);
+        }
+      }
+    };
+    xhr.send(data);
+  };
+
   const onSendMessage = (message: string) => {
     const newMessage = { role: "user", content: message };
-    // console.log("newmsg", newMessage);
     setMessages((prevMessages) => [...prevMessages, newMessage]);
   };
 
   useEffect(() => {
     console.log("Updated messages", messages);
+    postResult();
   }, [messages]);
 
   return (
