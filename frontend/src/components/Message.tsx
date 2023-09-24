ccac20093c3acdc116cb8ead8c88566af41e4e68
lixhuynh <elh7@rice.edu>
2023-09-24 03:59:42 -0500

    additional polishing

--- frontend/src/components/Message.tsx
+++ frontend/src/components/Message.tsx
@@ -4,7 +4,18 @@ export interface MessageProps {
 }
 
 function Message({ role, content }: MessageProps) {
-  return <div className={role + "-row"}><div className={role + "-msg"}>{content}</div></div>;
+  return (
+    <div className={role + "-row"}>
+      {role == "assistant" && (
+        <img
+          src="https://pluspng.com/img-png/book-png-book-stack-png-image-25686-1024.png"
+          width="75px"
+          height="75px"
+        />
+      )}
+      <div className={role + "-msg"}>{content}</div>
+    </div>
+  );
 }
 
 export default Message;
